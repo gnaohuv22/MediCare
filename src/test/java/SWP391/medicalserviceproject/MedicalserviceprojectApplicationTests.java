c764cb0bb71c17602529e0d19731f5cd9f3e7bd5
Phuong Nam <zariel.nd@gmail.com>
2023-09-06 22:07:43 +0700

    Spring Boot Framework installed

---
+++ src/test/java/SWP391/medicalserviceproject/MedicalserviceprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package SWP391.medicalserviceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalserviceprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
