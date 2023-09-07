7297deba7d287f91d3b345dee964ddcbf0dd7953
Phuong Nam <zariel.nd@gmail.com>
2023-09-07 15:58:25 +0700

    updated backend framework

--- backend/src/main/java/SWP391/backend/BackendApplication.java
+++ backend/src/main/java/SWP391/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package SWP391.medicalserviceproject;
+package SWP391.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MedicalserviceprojectApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MedicalserviceprojectApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
