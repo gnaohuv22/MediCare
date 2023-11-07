f26704542b29ec1b6113796e5fea287afd11ff33
Vu Hoang <hoangmeo1905@gmail.com>
2023-11-08 01:28:13 +0700

    add filter for reviews

--- MediCare/src/java/Controllers/DoctorReviewServlet.java
+++ MediCare/src/java/Controllers/DoctorReviewServlet.java
@@ -6,12 +6,18 @@ package Controllers;
 
 import DAL.ReviewDAO;
 import Models.Doctor;
+import Models.Reviews;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  *
@@ -22,27 +28,33 @@ public class DoctorReviewServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        int page = 1;
-        int recordsPerPage = 5;
         HttpSession session = request.getSession();
         if (session.getAttribute("doctorLoggedIn") == null) {
             session.setAttribute("error", "Bạn cần đăng nhập để truy cập trang này.");
             response.sendRedirect("user-login");
         } else {
-            if (request.getParameter("page") != null) {
-                page = Integer.parseInt(request.getParameter("page"));
-            }
+            
+            
             Doctor d = (Doctor) session.getAttribute("d");
             ReviewDAO rd = new ReviewDAO();
             
-            request.setAttribute("reviewList", rd.getReviewsByDoctorId(d.getId(), (page - 1) * recordsPerPage, recordsPerPage));
+            ArrayList<Reviews> reviewList = rd.getReviewsByDoctorId(d.getId());
+            Set<Double> uniqueRatingsSet = new HashSet<>();
+            Set<String> uniqueServicesSet = new HashSet<>();
             
-            int records = rd.getNumberRecord();
-            System.out.println("records: " + records);
-            int pages = (int) Math.ceil(records * 1.0 / recordsPerPage);
-
-            request.setAttribute("currentPage", page);
-            request.setAttribute("pages", pages);
+            for (Reviews r : reviewList) {
+                double rating = Double.parseDouble(r.getRating());
+                uniqueRatingsSet.add(Math.floor(rating));
+                uniqueServicesSet.add(r.getAppointment().getSt().getNametag());
+            }
+            List<Double> uniqueRatings = new ArrayList<>(uniqueRatingsSet);
+            Collections.sort(uniqueRatings);
+            
+            
+            
+            request.setAttribute("uniqueServicesSet", uniqueServicesSet);
+            request.setAttribute("uniqueRatings", uniqueRatings);
+            request.setAttribute("reviewList", reviewList);
             request.getRequestDispatcher("doctor-review.jsp").forward(request, response);
         }
     }
