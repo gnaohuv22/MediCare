fcbe473e5b1f48cedb3e2de1cc1f097e8c872187
Vu Hoang <hoangmeo1905@gmail.com>
2023-11-07 15:01:20 +0700

    update

--- MediCare/src/java/Controllers/DoctorReviewServlet.java
+++ MediCare/src/java/Controllers/DoctorReviewServlet.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package Controllers;
 
 import DAL.ReviewDAO;
@@ -19,26 +18,38 @@ import jakarta.servlet.http.HttpSession;
  * @author hoang
  */
 public class DoctorReviewServlet extends HttpServlet {
-   
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-         HttpSession session = request.getSession();
+            throws ServletException, IOException {
+        int page = 1;
+        int recordsPerPage = 5;
+        HttpSession session = request.getSession();
         if (session.getAttribute("doctorLoggedIn") == null) {
             session.setAttribute("error", "Bạn cần đăng nhập để truy cập trang này.");
             response.sendRedirect("user-login");
         } else {
+            if (request.getParameter("page") != null) {
+                page = Integer.parseInt(request.getParameter("page"));
+            }
             Doctor d = (Doctor) session.getAttribute("d");
             ReviewDAO rd = new ReviewDAO();
             
-            request.setAttribute("reviewList", rd.getReviewsByDoctorId(d.getId()));
+            request.setAttribute("reviewList", rd.getReviewsByDoctorId(d.getId(), (page - 1) * recordsPerPage, recordsPerPage));
+            
+            int records = rd.getNumberRecord();
+            System.out.println("records: " + records);
+            int pages = (int) Math.ceil(records * 1.0 / recordsPerPage);
+
+            request.setAttribute("currentPage", page);
+            request.setAttribute("pages", pages);
             request.getRequestDispatcher("doctor-review.jsp").forward(request, response);
         }
-    } 
+    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        
+            throws ServletException, IOException {
+
     }
 }
